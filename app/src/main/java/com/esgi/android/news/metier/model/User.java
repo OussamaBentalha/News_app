c4b47aa1dcbf2c854047eef01a4c14803f2f1d88
Oussama Bentalha <oussama.bentalha@gmail.com>
2016-06-21 19:14:23 +0200

    dev databese  - user

---
+++ app/src/main/java/com/esgi/android/news/metier/model/User.java
@@ -0,0 +1,44 @@
+package com.esgi.android.news.metier.model;
+
+/**
+ * Created by Sam on 21/06/16.
+ */
+public class User {
+
+    private long id;
+    private String password;
+    private String mail;
+
+    public User() {
+    }
+
+    public User(long id, String mail, String password) {
+        this.mail = mail;
+        this.id = id;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
